b101109b3139592076e89fa85f11fe851d732d85
alexhickey09 <70105276+alexhickey09@users.noreply.github.com>
2020-11-03 20:34:07 -0500

    Route ngo-choose-dc to dc-todaysfood and create event listeners for buttons

---
+++ client/ngo-choose-dc.js
@@ -0,0 +1,15 @@
+document.getElementById("frankbutton").addEventListener('click', () => {
+    window.location.href = "ngo-select-food.html";
+});
+
+document.getElementById("hampbutton").addEventListener('click', () => {
+    window.location.href = "ngo-select-food.html";
+});
+
+document.getElementById("woobutton").addEventListener('click', () => {
+    window.location.href = "ngo-select-food.html";
+});
+
+document.getElementById("berkbutton").addEventListener('click', () => {
+    window.location.href = "ngo-select-food.html";
+});
