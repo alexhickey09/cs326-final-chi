7e3a3d000ce8a05bb70e21bda59ff88dc9dbde2b
alexhickey09 <70105276+alexhickey09@users.noreply.github.com>
2020-11-03 22:12:58 -0500

    Linter fixes

--- client/ngo-select-food.js
+++ client/ngo-select-food.js
@@ -1,20 +1,20 @@
 window.addEventListener("load", function() {
-    let table = document.querySelector("table");
-    let thead = table.createTHead();
-    let header = thead.insertRow();
+    const table = document.querySelector("table");
+    const thead = table.createTHead();
+    const header = thead.insertRow();
     
-    let nameHead = document.createElement("th");
-    let nameHeadText = document.createTextNode("Name");
+    const nameHead = document.createElement("th");
+    const nameHeadText = document.createTextNode("Name");
     nameHead.appendChild(nameHeadText);
     header.appendChild(nameHead);
 
-    let categoryHead = document.createElement("th");
-    let categoryHeadText = document.createTextNode("Category");
+    const categoryHead = document.createElement("th");
+    const categoryHeadText = document.createTextNode("Category");
     categoryHead.appendChild(categoryHeadText);
     header.appendChild(categoryHead);
 
-    let amountHead = document.createElement("th");
-    let amountHeadText = document.createTextNode("Amount");
+    const amountHead = document.createElement("th");
+    const amountHeadText = document.createTextNode("Amount");
     amountHead.appendChild(amountHeadText);
     header.appendChild(amountHead);
    
@@ -25,19 +25,19 @@ window.addEventListener("load", function() {
             console.log(data);
             
             for(let i = 0; i < data.length; i++) {
-                let row = table.insertRow();
+                const row = table.insertRow();
 
-                let name = row.insertCell();
-                let nameText = document.createTextNode(data[i].name);
+                const name = row.insertCell();
+                const nameText = document.createTextNode(data[i].name);
                 name.appendChild(nameText);
 
-                let category = row.insertCell();
-                let categoryText = document.createTextNode(data[i].category);
+                const category = row.insertCell();
+                const categoryText = document.createTextNode(data[i].category);
                 category.appendChild(categoryText);
 
-                let amount = row.insertCell();
-                let amountText = document.createTextNode(data[i].amount);
+                const amount = row.insertCell();
+                const amountText = document.createTextNode(data[i].amount);
                 amount.appendChild(amountText);
             }
         });
-})
+});
