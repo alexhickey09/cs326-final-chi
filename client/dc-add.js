7dcf16d063b0f561145f45f06e5e97366a6051c0
alexhickey09 <70105276+alexhickey09@users.noreply.github.com>
2020-11-03 21:53:30 -0500

    Finish addfood post endpoint

--- client/dc-add.js
+++ client/dc-add.js
@@ -1,10 +1,10 @@
 document.getElementById("addfoodbutton").addEventListener('click', async function() {
-    const foodItem = {"name": document.getElementById("name"),
-                      "category": document.getElementById("category"),
-                      "amount": document.getElementById("amount"),
-                      "nutrition": document.getElementById("nutrition")};
+    const foodItem = {"name": document.getElementById("name").value,
+                      "category": document.getElementById("category").value,
+                      "amount": document.getElementById("amount").value,
+                      "nutrition": document.getElementById("nutrition").value};
 
-    fetch('http://localhost:8080/wordScore', {
+    fetch('http://localhost:8080/addfood', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json;charset=utf-8'
