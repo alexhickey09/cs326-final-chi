d24d3f0240380fdedd684f78542c9b09546949d3
alexhickey09 <70105276+alexhickey09@users.noreply.github.com>
2020-11-03 20:34:54 -0500

    Create routing for dc-home and add event listeners to buttons

---
+++ client/dc-home.js
@@ -0,0 +1,16 @@
+document.getElementById("frankdcbutton").addEventListener('click', () => {
+    console.log("Here");
+    window.location.href = "dc-today'sfood.html";
+});
+
+document.getElementById("hampdcbutton").addEventListener('click', () => {
+    window.location.href = "dc-today'sfood.html";
+});
+
+document.getElementById("woodcbutton").addEventListener('click', () => {
+    window.location.href = "dc-today'sfood.html";
+});
+
+document.getElementById("berkdcbutton").addEventListener('click', () => {
+    window.location.href = "dc-today'sfood.html";
+});
