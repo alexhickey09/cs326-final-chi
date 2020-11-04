0030789db489bb994a2914cbe252e8aaaff194a7
alexhickey09 <70105276+alexhickey09@users.noreply.github.com>
2020-11-03 22:22:21 -0500

    Add rerouting to contact and request pages

--- client/dc-todaysfood.js
+++ client/dc-todaysfood.js
@@ -1,3 +1,11 @@
 document.getElementById("add-food-button").addEventListener('click', () => {
     window.location.href = "dc-add.html";
 });
+
+document.getElementById("update-contact-button").addEventListener('click', () => {
+    window.location.href = "dc-update.html";
+});
+
+document.getElementById("requests-button").addEventListener('click', () => {
+    window.location.href = "dc-requests.html";
+});
