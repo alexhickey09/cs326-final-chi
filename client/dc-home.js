70a15aaaf3e467b23a2b9ad35685149791b181c9
alexhickey09 <70105276+alexhickey09@users.noreply.github.com>
2020-11-03 20:51:10 -0500

    Remove apostrophes in dc-todaysfood event listeners

--- client/dc-home.js
+++ client/dc-home.js
@@ -1,16 +1,16 @@
 document.getElementById("frankdcbutton").addEventListener('click', () => {
     console.log("Here");
-    window.location.href = "dc-today'sfood.html";
+    window.location.href = "dc-todaysfood.html";
 });
 
 document.getElementById("hampdcbutton").addEventListener('click', () => {
-    window.location.href = "dc-today'sfood.html";
+    window.location.href = "dc-todaysfood.html";
 });
 
 document.getElementById("woodcbutton").addEventListener('click', () => {
-    window.location.href = "dc-today'sfood.html";
+    window.location.href = "dc-todaysfood.html";
 });
 
 document.getElementById("berkdcbutton").addEventListener('click', () => {
-    window.location.href = "dc-today'sfood.html";
+    window.location.href = "dc-todaysfood.html";
 });
