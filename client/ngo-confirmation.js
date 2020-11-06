955a7952bd0740cda2c81615bd886ab956f36174
alex preston <ajpreston@umass.edu>
2020-11-06 15:52:29 -0500

    added functionality for confirm button

--- client/ngo-confirmation.js
+++ client/ngo-confirmation.js
@@ -46,3 +46,26 @@ window.addEventListener("load", function() {
 document.getElementById("back-to-food-select").addEventListener('click', function() {
     window.location.href = "ngo-select-food.html";
 });
+
+document.getElementById("submitreq").addEventListener('click', function() {
+    fetch('https://dishsaver.herokuapp.com/viewrequests')
+        .then(response => response.json())
+        .then(data => {
+            for(let i = 0; i < data.length; i++) {
+                const row = table.insertRow();
+
+                const name = row.insertCell();
+                const nameText = document.createTextNode(data[i].name);
+                name.appendChild(nameText);
+
+                const category = row.insertCell();
+                const categoryText = document.createTextNode(data[i].category);
+                category.appendChild(categoryText);
+
+                const amount = row.insertCell();
+                const amountText = document.createTextNode(data[i].amount);
+                amount.appendChild(amountText);
+            }
+        });
+    window.location.href = "ngo-select-food.html";
+});
