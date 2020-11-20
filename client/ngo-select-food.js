96de305eb3fc529f9a08db98e40f888447da29af
alexhickey09 <70105276+alexhickey09@users.noreply.github.com>
2020-11-20 14:33:19 -0500

    Make food and selection tables only show up when there is food or a selection available

--- client/ngo-select-food.js
+++ client/ngo-select-food.js
@@ -1,70 +1,74 @@
-window.addEventListener("load", function() {
-    //Making the table and the header row
-    const table = document.querySelector("table");
-    const thead = table.createTHead();
-    const header = thead.insertRow();
-    
-    const nameHead = document.createElement("th");
-    nameHead.classList.add("infocol");
-    const nameHeadText = document.createTextNode("Name");
-    nameHead.appendChild(nameHeadText);
-    header.appendChild(nameHead);
-
-    const categoryHead = document.createElement("th");
-    categoryHead.classList.add("infocol");
-    const categoryHeadText = document.createTextNode("Category");
-    categoryHead.appendChild(categoryHeadText);
-    header.appendChild(categoryHead);
-
-    const amountHead = document.createElement("th");
-    amountHead.classList.add("infocol");
-    const amountHeadText = document.createTextNode("Amount");
-    amountHead.appendChild(amountHeadText);
-    header.appendChild(amountHead);
-
-    const selectHead = document.createElement("th");
-    selectHead.classList.add("selectcol");
-    const selectHeadText = document.createTextNode("Select");
-    selectHead.appendChild(selectHeadText);
-    header.appendChild(selectHead);
-   
+window.addEventListener("load", function() {   
     //Making an additional row for each food item that has been listed
     fetch('/viewfood?dc=' + window.localStorage.getItem('dc'))
         .then(response => response.json())
         .then(data => {
-            for(let i = 0; i < data.length; i++) {
-                const row = table.insertRow();
+            if(data.length > 0) {
+                //Making the table and the header row
+                const table = document.querySelector("table");
+                const thead = table.createTHead();
+                const header = thead.insertRow();
+                
+                const nameHead = document.createElement("th");
+                nameHead.classList.add("infocol");
+                const nameHeadText = document.createTextNode("Name");
+                nameHead.appendChild(nameHeadText);
+                header.appendChild(nameHead);
 
-                const name = row.insertCell();
-                const nameText = document.createTextNode(data[i].name);
-                name.appendChild(nameText);
+                const categoryHead = document.createElement("th");
+                categoryHead.classList.add("infocol");
+                const categoryHeadText = document.createTextNode("Category");
+                categoryHead.appendChild(categoryHeadText);
+                header.appendChild(categoryHead);
 
-                const category = row.insertCell();
-                const categoryText = document.createTextNode(data[i].category);
-                category.appendChild(categoryText);
+                const amountHead = document.createElement("th");
+                amountHead.classList.add("infocol");
+                const amountHeadText = document.createTextNode("Amount");
+                amountHead.appendChild(amountHeadText);
+                header.appendChild(amountHead);
 
-                const amount = row.insertCell();
-                const amountText = document.createTextNode(data[i].amount);
-                amount.appendChild(amountText);
+                const selectHead = document.createElement("th");
+                selectHead.classList.add("selectcol");
+                const selectHeadText = document.createTextNode("Select");
+                selectHead.appendChild(selectHeadText);
+                header.appendChild(selectHead);
+                for(let i = 0; i < data.length; i++) {
+                    const row = table.insertRow();
 
-                const select = row.insertCell();
-                select.type = "button";
-                select.className = "btn btn-secondary";
-                select.innerHTML = "Select";
-                select.onclick = (function selectFood() {
-                    const selection = {
-                        name: data[i].name,
-                        category: data[i].category,
-                        amount: data[i].amount
-                    };
-                    fetch('./addToSelection', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json;charset=utf-8'
-                        },
-                        body: JSON.stringify(selection),
+                    const name = row.insertCell();
+                    const nameText = document.createTextNode(data[i].name);
+                    name.appendChild(nameText);
+
+                    const category = row.insertCell();
+                    const categoryText = document.createTextNode(data[i].category);
+                    category.appendChild(categoryText);
+
+                    const amount = row.insertCell();
+                    const amountText = document.createTextNode(data[i].amount);
+                    amount.appendChild(amountText);
+
+                    const select = row.insertCell();
+                    select.type = "button";
+                    select.className = "btn btn-secondary";
+                    select.innerHTML = "Select";
+                    select.onclick = (function selectFood() {
+                        const selection = {
+                            name: data[i].name,
+                            category: data[i].category,
+                            amount: data[i].amount
+                        };
+                        fetch('./addToSelection', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json;charset=utf-8'
+                            },
+                            body: JSON.stringify(selection),
+                        });
                     });
-                });
+                }
+            }
+            else {
+                document.getElementById("no-food").innerHTML = "Sorry, there is currently no food listed. Please check back later.";
             }
         });
 
