30853e62fcc7242c3319b55b911e8a35ef259393
alexhickey09 <70105276+alexhickey09@users.noreply.github.com>
2020-11-20 14:14:14 -0500

    Show current manager details on update contact info screen

--- client/dc-update.js
+++ client/dc-update.js
@@ -1,3 +1,30 @@
+window.addEventListener("load", function () {
+    fetch('/viewcontact?dc=' + window.localStorage.getItem('dc'))
+        .then(response => response.json())
+        .then(data => {
+            const contactInfo = document.createElement("h2");
+            contactInfo.classList.add("DC");
+            contactInfo.innerHTML = "Current Manager Contact Info";
+            document.body.appendChild(contactInfo);
+
+            const name = document.createElement("div");
+            name.classList.add("DC");
+            name.innerHTML = data.name;
+            document.body.appendChild(name);
+
+            const email = document.createElement("div");
+            email.classList.add("DC");
+            email.innerHTML = data.email;
+            document.body.appendChild(email);
+
+            const phone = document.createElement("div");
+            phone.classList.add("DC");
+            phone.innerHTML = data.phone;
+            document.body.appendChild(phone);
+            
+        });
+});
+
 document.getElementById("update-contact-button").addEventListener('click', function() {
     const contact = {"name": document.getElementById("name").value,
                      "email": document.getElementById("email").value,
